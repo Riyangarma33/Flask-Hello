e7eac7e92cf55531aafe354fa1f8b4d4a76c3af1
Muhammad Garma <mgarma76@gmail.com>
2023-06-06 14:05:53 +0700

    Add eval msg

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from asgiref.wsgi import WsgiToAsgi
 from flask import Flask
+from ast import literal_eval
 from os import environ
 
 app = Flask(__name__)
 
 @app.route('/')
 async def hello() -> str:
-    return environ.get('message') if 'message' in environ else 'Hello World!'
+    ret_msg : str
+    if 'message' in environ:
+        ret_msg = environ.get('message')
+        try: 
+            eval_msg = literal_eval(ret_msg)
+            ret_msg += f'\n{eval_msg}'
+        except ValueError as err:
+            ret_msg += f'\n{err}'
+    else:
+        ret_msg = 'Hello World!'
+    return ret_msg
 
 asgi_app = WsgiToAsgi(app)
 
